d84000a99a41cfd19e9be81aae4d268a9a9de959
Joseph Cardona <121201452+Joseph-Cardona@users.noreply.github.com>
2026-05-24 22:21:42 -0700

    Update CORS origin for production environment

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 const SECRET = process.env.JWT_SECRET || 'supersecret';
 
 app.use(cors({
-  origin: ['https://comunidad-spanish.vercel.app', 'http://localhost:5173'],
+  origin: ['https://comunidad-spanish-h0pj7w0dw-joseph-cardonas-projects.vercel.app/', 'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
