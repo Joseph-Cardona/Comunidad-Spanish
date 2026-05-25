c9e2c38eebc8b25ea336b4a7d1cf5497f951ea6a
Joseph Cardona <121201452+Joseph-Cardona@users.noreply.github.com>
2026-05-24 22:00:52 -0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
   define: {
-    'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL)
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL || 'https://comunidad-spanish.onrender.com/')
   }
-})
+});
